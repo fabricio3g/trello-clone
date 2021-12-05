7dd00969c5155f437faeb5db46842fcaf249c2d5
fabricio3g <fabricio.3g@gmail.com>
2021-12-05 09:20:30 -0300

    Update Main.tsx

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {AppContainer} from './Styles'
+import {AppContainer} from '../Styles'
 
 import { Column } from "./Column";
 import { Card } from "./Card";
@@ -16,6 +16,11 @@ class Main extends Component{
                 <Column text="Todos">
                     <Card text="Walk the dog"/>
                 </Column>
+                <Column text="Todos">
+                    <Card text="Walk the dog"/>
+                 <AddNewItem toggleButtonText="+ Add Another list" dark onAdd={console.log}/>     
+                </Column>
+                
                 <AddNewItem toggleButtonText="+ Add Another list" onAdd={console.log}/>
             </AppContainer>
         )
