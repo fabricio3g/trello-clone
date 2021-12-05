fdaea98997ba481348d952ff32558e339fd18270
fabricio3g <fabricio.3g@gmail.com>
2021-12-05 09:20:44 -0300

    useFocus added

--- src/Components/NewFormItem.tsx
+++ src/Components/NewFormItem.tsx
@@ -1,5 +1,6 @@
 import React,{useState} from "react";
-import { NewItemFormContainer, NewItemButton, NewItemInput } from "./Styles";
+import { NewItemFormContainer, NewItemButton, NewItemInput } from "../Styles";
+import { useFocus } from '../utils/useFocus'
 
 interface NewItemFormProps {
     onAdd(text:string):void
@@ -9,10 +10,12 @@ interface NewItemFormProps {
 export const NewFormItem =(props:NewItemFormProps)=>{
     const [text, setText] = useState("")
     const { onAdd } = props
-
+    const inputRef = useFocus()
     return(
         <NewItemFormContainer>
-            <NewItemInput value={text} 
+            <NewItemInput 
+                value={text}
+                ref={inputRef} 
                 onChange={e => setText(e.target.value)}
             />
             <NewItemButton onClick={()=> onAdd(text)}>
