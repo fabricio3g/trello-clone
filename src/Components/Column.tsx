e5ef1e098c34500ade2a9a79a268d9af2af3768d
fabricio3g <fabricio.3g@gmail.com>
2021-12-06 22:02:20 -0300

    custom hook to grab data

--- src/Components/Column.tsx
+++ src/Components/Column.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Card } from './Card'
 import {ColumnContainer, ColumnTitle} from '../Styles'
+import { AddNewItem } from './AddNewItem'
+
+import { useAppState } from '../state/AppStateContext'
 
 interface ColumnProps{
     text: string
+    id:string
 }
 
 
@@ -11,13 +15,25 @@ type PropsWithChildren<P> = P & {
     children?: React.ReactNode;
     }
 
-export const Column =({text, children}: PropsWithChildren<ColumnProps>)=>{
+export const Column =({text, id}: PropsWithChildren<ColumnProps>)=>{
 
+    const { getTasksById } = useAppState()
+
+    const tasks = getTasksById(id)
 
     return(
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            {children}
+            {
+            tasks.map( (task: any) => (
+                <Card text={task.text} key={task.id} id={task.id}></Card>
+            ))       
+            }
+            <AddNewItem
+                toggleButtonText="+ Add another task"
+                onAdd={console.log}
+                dark
+            />
         </ColumnContainer>
     )
 }
